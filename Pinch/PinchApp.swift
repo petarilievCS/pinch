64a827fc005048b2fcf05a73f4ed70ad7cd517a3
petar.iliev <petar.iliev@servicenow.com>
2023-06-15 09:48:14 -0700

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Petar Iliev on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
