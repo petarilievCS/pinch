434e212bc30039f6204a4cd7f9e8d99a47597059
petar.iliev <petar.iliev@servicenow.com>
2023-06-15 10:17:50 -0700

    Learned Tap Gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - PROPERTIES
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.easeOut, value: isAnimating)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2) {
+                        withAnimation(.spring()) {
+                            imageScale = imageScale == 3.0 ? 1.0 : 3.0
+                        }
+                    }
+            } //: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                isAnimating = true
+            }
+        } //: NAVIGATION STACK
     }
 }
 
