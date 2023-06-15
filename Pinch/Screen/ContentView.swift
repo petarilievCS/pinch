b2af45e5d7b277e4f7e607d0ccc4b0703ed81e82
petar.iliev <petar.iliev@servicenow.com>
2023-06-15 10:55:57 -0700

    Learned Drag Gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     // MARK: - PROPERTIES
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1.0
+    @State private var imageOffset: CGSize = .zero
     
     var body: some View {
         NavigationStack {
@@ -24,13 +25,29 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(imageOffset)
                     .animation(.easeOut, value: isAnimating)
                     .scaleEffect(imageScale)
                     .onTapGesture(count: 2) {
                         withAnimation(.spring()) {
                             imageScale = imageScale == 3.0 ? 1.0 : 3.0
+                            imageOffset = .zero
                         }
                     }
+                    .gesture(DragGesture()
+                        .onChanged({ value in
+                            withAnimation(.linear(duration: 1)) {
+                                imageOffset = value.translation
+                            }
+                            
+                        })
+                            .onEnded({ _ in
+                                withAnimation(.linear) {
+                                    if imageScale == 1.0 { imageOffset = .zero }
+                                }
+                                
+                            })
+                    )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
